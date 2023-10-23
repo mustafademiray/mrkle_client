fc259d1a2b6aa7282e27594ab08ec095c7b450ce
mustafademiray <mustafademiray@protonmail.com>
2023-10-23 13:03:41 +0300

    merkle tree update

--- src/main.rs
+++ src/main.rs
@@ -39,6 +39,17 @@ impl MerkleNode {
             right: Some(Box::new(right)),
         }
     }
+    fn get_parent(&self) -> Option<&MerkleNode> {
+        self.parent.as_deref()
+    }
+
+    fn get_left(&self) -> Option<&MerkleNode> {
+        self.left.as_deref()
+    }
+
+    fn get_right(&self) -> Option<&MerkleNode> {
+        self.right.as_deref()
+    }
 }
 
 
@@ -56,6 +67,8 @@ impl MerkleTree {
     fn generate_merkle_tree(data: &[String]) -> Option<Box<MerkleNode>> {
         let leaf_nodes: Vec<MerkleNode> = data.iter().map(|hash| MerkleNode::new(hash)).collect();
 
+        //println!("Leaf nodes are: {:?}", leaf_nodes);
+
         let mut nodes: Vec<MerkleNode> = leaf_nodes.clone();
 
         while nodes.len() > 1 {
@@ -71,6 +84,8 @@ impl MerkleTree {
             }
             nodes = new_nodes;
         }
+
+        //println!("Generated Nodes After Loop: {:?}", nodes);
 
         if let Some(root) = nodes.pop() {
             Some(Box::new(root))
@@ -225,65 +240,71 @@ fn upload_file(file_path: &str) -> UploadResult {
     }
 }
 
-fn generate_merkle_proof(merkle_tree: &MerkleTree, index_to_verify: usize) -> Option<Vec<(String, String)>> {
+
+fn generate_merkle_proof(merkle_tree: &MerkleTree, index_to_verify: usize) -> Option<Vec<(String, String)>>
+{
     let mut proof: Vec<(String, String)> = Vec::new();
-    
     let mut current = &merkle_tree.root;
     let mut index = index_to_verify;
-    
-    while index > 0 {
-        // Determine the parent index
-        let parent_index = (index - 1) / 2;
-        
-        // Use pattern matching to get the child node
-        let parent_node = match &current {
-            Some(node) => {
-                if parent_index % 2 == 0 {
-                    // The node is the left child of the parent
-                    &node.left
-                } else {
-                    // The node is the right child of the parent
-                    &node.right
-                }
+
+    while index > 0
+    {
+        let is_left_child = index % 2 == 0;
+        let sibling_index = if is_left_child { index - 1 } else { index + 1 };
+        let sibling_node = if let Some(parent) = current.get_parent()
+        {
+            if is_left_child {
+                parent.get_right()
             }
-            None => return None, // Proof generation failed
-        };
-        
-        if parent_index % 2 == 0 {
-            if let Some(node) = &parent_node {
-                proof.push(("right".to_string(), node.hash.clone()));
-            } else {
-                return None; // Proof generation failed
-            }
-        } else {
-            if let Some(node) = &parent_node {
-                proof.push(("left".to_string(), node.hash.clone()));
-            } else {
-                return None; // Proof generation failed
+            else {
+                parent.get_left()
             }
         }
-        
-        current = parent_node; // Move up one level
-        index = parent_index; // Update the current index
+        else
+        {
+            return None;
+        };
+
+        match sibling_node
+        {
+            Some(node) =>
+            {
+                proof.push((if is_left_child { "right" } else { "left" }, node.hash.clone()));
+            }
+            None => return None,
+        }
+
+        current = if let Some(parent) = current.get_parent()
+        {
+            parent
+        }
+        else
+        {
+            return None;
+        };
+
+        index = (index - 1) / 2;
     }
-    
+
     Some(proof)
 }
 
 
 
+
 fn verify_hash_using_proof(merkle_proof: &[(String, String)], hash_to_verify: &str) -> bool {
     let mut hash = hash_to_verify.to_string();
-    let mut position = "right"; // Initialize the position as "right".
+    let mut position = "left";  // Initialize the position as "left".
 
+    println!("We want to verify this: {:?}", hash_to_verify);
     for (proof_position, sibling_hash) in merkle_proof {
         println!("Position: {}, Sibling Hash: {}, Current Hash: {}", position, sibling_hash, hash);
         if proof_position == "left" {
             hash = hash_data(&format!("{}{}", sibling_hash, hash));
-            position = "left";
+            position = "right"; // Reverse the position
         } else {
             hash = hash_data(&format!("{}{}", hash, sibling_hash));
-            position = "right";
+            position = "left"; // Reverse the position
         }
         println!("Updated Hash: {}", hash);
     }
@@ -296,6 +317,7 @@ fn verify_hash_using_proof(merkle_proof: &[(String, String)], hash_to_verify: &s
 
     verification_result
 }
+
 
 
 
@@ -341,12 +363,12 @@ fn main() -> Result<(), IoError> {
 
 
 
-        ///NEW MERKLE TREE
+        //NEW MERKLE TREE
         let merkle_tree_new = MerkleTree::new(content_hashes.clone());
 
         // Choose an index to verify (0 in this case)
         let index_to_verify = 2;
-        println!("Root: {:?}", root);
+        //println!("Root: {:?}", root);
         // Generate the Merkle proof for the specified index_to_verify
         if let Some(merkle_proof) = generate_merkle_proof(&merkle_tree_new, index_to_verify) {
             println!("Merkle Proof: {:?}", merkle_proof);
