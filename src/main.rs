65444aae80b855e24667c38219c4225a84ead7ad
mustafademiray <mustafademiray@protonmail.com>
2023-10-24 14:24:51 +0300

    merklenode/merkletree update

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,9 @@ use reqwest::blocking::Client;
 use reqwest::blocking::multipart::Form;
 use sha2::{Digest, Sha256};
 use std::fs::File;
-use std::io::{Read, Error as IoError, Result as IoResult};
+use std::io::{Error as IoError, Read, Result as IoResult};
 use std::path::Path;
 use std::fs;
-use std::string::String;
 
 enum UploadResult {
     Success,
@@ -15,84 +14,180 @@ enum UploadResult {
 #[derive(Debug, Clone)]
 struct MerkleNode {
     hash: String,
-    left: Option<Box<MerkleNode>>,
-    right: Option<Box<MerkleNode>>,
+    left: Option<usize>,
+    right: Option<usize>,
+    parent: Option<usize>,
+    is_leaf: bool,
 }
 
-
-
 impl MerkleNode {
-    fn new(data: &str) -> MerkleNode {
-        let hash = data.to_string();
-        MerkleNode {
+    fn new_leaf(data: &str) -> Result<MerkleNode, String> {
+        let hash = hash_data(data);
+        Ok(MerkleNode {
             hash,
             left: None,
             right: None,
-        }
+            parent: None,
+            is_leaf: true,
+        })
     }
 
-    fn combine(left: MerkleNode, right: MerkleNode) -> MerkleNode {
+    fn new_internal(left: MerkleNode, right: MerkleNode) -> Result<MerkleNode, String> {
+        if left.parent.is_none() || right.parent.is_none() {
+            return Err("Parent reference missing for child nodes".to_string());
+        }
+
         let combined_hash = hash_data(&format!("{}{}", left.hash, right.hash));
-        MerkleNode {
+        Ok(MerkleNode {
             hash: combined_hash,
-            left: Some(Box::new(left)),
-            right: Some(Box::new(right)),
+            left: Some(left.parent.unwrap()),
+            right: Some(right.parent.unwrap()),
+            parent: left.parent, // Set the parent reference for the new internal node
+            is_leaf: false,
+        })
+    }
+}
+
+
+impl Default for MerkleNode {
+    fn default() -> Self {
+        MerkleNode {
+            hash: String::new(),
+            left: None,
+            right: None,
+            parent: None,
+            is_leaf: false,
         }
     }
-    fn get_parent(&self) -> Option<&MerkleNode> {
-        self.parent.as_deref()
-    }
-
-    fn get_left(&self) -> Option<&MerkleNode> {
-        self.left.as_deref()
-    }
-
-    fn get_right(&self) -> Option<&MerkleNode> {
-        self.right.as_deref()
-    }
 }
 
 
+#[derive(Debug, Clone)]
 struct MerkleTree {
-    root: Option<Box<MerkleNode>>,
+    root: Option<usize>,
+    nodes: Vec<MerkleNode>,
 }
-
 
 impl MerkleTree {
     fn new(data: Vec<String>) -> MerkleTree {
-        let root = MerkleTree::generate_merkle_tree(&data);
-        MerkleTree { root }
+        let (root, nodes) = MerkleTree::generate_merkle_tree(&data, Vec::new(), None);
+    
+        let root_index = root.unwrap_or(0); // Set root to 0 if None
+        MerkleTree {
+            root: Some(root_index),
+            nodes,
+        }
     }
 
-    fn generate_merkle_tree(data: &[String]) -> Option<Box<MerkleNode>> {
-        let leaf_nodes: Vec<MerkleNode> = data.iter().map(|hash| MerkleNode::new(hash)).collect();
-
-        //println!("Leaf nodes are: {:?}", leaf_nodes);
-
-        let mut nodes: Vec<MerkleNode> = leaf_nodes.clone();
-
-        while nodes.len() > 1 {
-            let mut new_nodes = Vec::new();
-            for i in (0..nodes.len()).step_by(2) {
-                let left = nodes[i].clone();
-                let right = if i + 1 < nodes.len() {
-                    nodes[i + 1].clone()
-                } else {
-                    left.clone()
-                };
-                new_nodes.push(MerkleNode::combine(left, right));
+    fn generate_merkle_tree(data: &[String], mut nodes: Vec<MerkleNode>, parent: Option<usize>) -> (Option<usize>, Vec<MerkleNode>) {
+        if data.is_empty() {
+            return (None, nodes);
+        }
+    
+        let node_index = nodes.len();
+        
+        let new_node = if data.len() == 1 {
+            match MerkleNode::new_leaf(&data[0]) {
+                Ok(mut leaf_node) => {
+                    leaf_node.parent = parent; // Set the parent reference for the leaf node
+                    leaf_node
+                }
+                Err(err_message) => {
+                    println!("Error creating leaf node: {}", err_message);
+                    return (None, nodes);
+                }
             }
-            nodes = new_nodes;
-        }
-
-        //println!("Generated Nodes After Loop: {:?}", nodes);
-
-        if let Some(root) = nodes.pop() {
-            Some(Box::new(root))
         } else {
-            None
+            match MerkleNode::new_internal(MerkleNode::default(), MerkleNode::default()) {
+                Ok(mut internal_node) => {
+                    internal_node.parent = parent; // Set the parent reference for the internal node
+                    internal_node
+                }
+                Err(err_message) => {
+                    println!("Error creating internal node: {}", err_message);
+                    return (None, nodes);
+                }
+            }
+        };
+    
+        nodes.push(new_node);
+    
+        if data.len() > 1 {
+            let left_index = node_index;
+            let (right_index, new_nodes) = MerkleTree::generate_merkle_tree(&data[1..].to_vec(), nodes.clone(), Some(node_index));
+            nodes[left_index].parent = Some(node_index); // Set parent for left child
+            nodes[node_index].left = Some(left_index);
+            nodes[node_index].right = Some(right_index.unwrap());
+            nodes[node_index].is_leaf = false;
+            nodes.extend_from_slice(&new_nodes);
         }
+    
+        if nodes[node_index].is_leaf {
+            nodes.push(nodes[node_index].clone());
+        } else {
+            let left_index = nodes[node_index].left;
+            let right_index = nodes[node_index].right;
+    
+            if let (Some(left_idx), Some(right_idx)) = (left_index, right_index) {
+                match MerkleNode::new_internal(nodes[left_idx].clone(), nodes[right_idx].clone()) {
+                    Ok(mut new_internal_node) => {
+                        new_internal_node.parent = parent; // Set the parent reference for the new internal node
+                        nodes.push(new_internal_node);
+                    }
+                    Err(err_message) => {
+                        println!("Error creating internal node: {}", err_message);
+                        return (None, nodes);
+                    }
+                }
+            } else {
+                println!("Error: left or right is None");
+            }
+        }
+    
+        (Some(node_index), nodes)
     }
+    
+    
+        
+
+    fn generate_merkle_proof(&self, index_to_verify: usize) -> Option<Vec<(String, String)>> {
+        if index_to_verify >= self.nodes.len() {
+            return None; // Index is out of bounds
+        }
+    
+        let mut proof: Vec<(String, String)> = Vec::new();
+        let mut current_index = index_to_verify;
+    
+        while let Some(parent_index) = self.nodes[current_index].parent {
+            let parent = &self.nodes[parent_index];
+    
+            let sibling_index = if parent.left == Some(current_index) {
+                parent.right
+            } else {
+                parent.left
+            };
+    
+            if let Some(sibling_index) = sibling_index {
+                let sibling = &self.nodes[sibling_index];
+                proof.push(("sibling".to_string(), sibling.hash.clone()));
+            } else {
+                // Handle the case where the sibling is missing (you can choose how to handle this)
+                // Here, we just add an empty string as a placeholder.
+                proof.push(("sibling".to_string(), String::new()));
+            }
+    
+            if parent.left == Some(current_index) {
+                proof.push(("position".to_string(), "left".to_string()));
+            } else {
+                proof.push(("position".to_string(), "right".to_string()));
+            }
+    
+            current_index = parent_index;
+        }
+    
+        Some(proof)
+    }
+    
 }
 
 fn hash_data(data: &str) -> String {
@@ -101,33 +196,6 @@ fn hash_data(data: &str) -> String {
     let result = hasher.finalize();
     format!("{:x}", result)
 }
-
-
-fn generate_merkle_tree(data: &Vec<String>) -> MerkleNode {
-    // Create leaf nodes from the data
-    let leaf_nodes: Vec<MerkleNode> = data.iter().map(|hash| MerkleNode::new(hash)).collect();
-
-    // Build the Merkle Tree by combining nodes
-    let mut nodes: Vec<MerkleNode> = leaf_nodes.clone();
-
-    while nodes.len() > 1 {
-        let mut new_nodes = Vec::new();
-        for i in (0..nodes.len()).step_by(2) {
-            let left = nodes[i].clone();
-            let right = if i + 1 < nodes.len() {
-                nodes[i + 1].clone()
-            } else {
-                left.clone() // For an odd number of nodes, duplicate the last node as the right child
-            };
-            new_nodes.push(MerkleNode::combine(left, right));
-        }
-        nodes = new_nodes;
-    }
-
-    nodes[0].clone() // Return the root of the Merkle Tree
-}
-
-
 
 fn hash_content(file_path: &str) -> IoResult<String> {
     let mut file = File::open(file_path)?;
@@ -142,7 +210,7 @@ fn hash_content(file_path: &str) -> IoResult<String> {
         }
         hasher.update(&buffer[..bytes_read]);
     }
-    
+
     let result = hasher.finalize();
     let hash = result.iter().map(|byte| format!("{:02x}", byte)).collect::<String>();
     Ok(hash)
@@ -192,9 +260,10 @@ fn upload_file(file_path: &str) -> UploadResult {
         }
     };
     println!("File hash: {:?}", content_hash);
+
     // Initialize the reqwest client
     let client = Client::new();
-    
+
     // Read the file data
     if let Ok(mut file) = File::open(&file_path) {
         let mut file_data = Vec::new();
@@ -217,7 +286,7 @@ fn upload_file(file_path: &str) -> UploadResult {
             {
                 // Check the server response
                 if response.status().is_success() {
-                                        // Remove the file after successful upload
+                    // Remove the file after successful upload
                     if let Err(err) = std::fs::remove_file(&file_path) {
                         eprintln!("Failed to remove the local file: {}", err);
                     }
@@ -240,96 +309,28 @@ fn upload_file(file_path: &str) -> UploadResult {
     }
 }
 
-
-fn generate_merkle_proof(merkle_tree: &MerkleTree, index_to_verify: usize) -> Option<Vec<(String, String)>>
-{
-    let mut proof: Vec<(String, String)> = Vec::new();
-    let mut current = &merkle_tree.root;
-    let mut index = index_to_verify;
-
-    while index > 0
-    {
-        let is_left_child = index % 2 == 0;
-        let sibling_index = if is_left_child { index - 1 } else { index + 1 };
-        let sibling_node = if let Some(parent) = current.get_parent()
-        {
-            if is_left_child {
-                parent.get_right()
-            }
-            else {
-                parent.get_left()
-            }
-        }
-        else
-        {
-            return None;
-        };
-
-        match sibling_node
-        {
-            Some(node) =>
-            {
-                proof.push((if is_left_child { "right" } else { "left" }, node.hash.clone()));
-            }
-            None => return None,
-        }
-
-        current = if let Some(parent) = current.get_parent()
-        {
-            parent
-        }
-        else
-        {
-            return None;
-        };
-
-        index = (index - 1) / 2;
-    }
-
-    Some(proof)
-}
-
-
-
-
 fn verify_hash_using_proof(merkle_proof: &[(String, String)], hash_to_verify: &str) -> bool {
     let mut hash = hash_to_verify.to_string();
-    let mut position = "left";  // Initialize the position as "left".
-
-    println!("We want to verify this: {:?}", hash_to_verify);
+    
     for (proof_position, sibling_hash) in merkle_proof {
-        println!("Position: {}, Sibling Hash: {}, Current Hash: {}", position, sibling_hash, hash);
         if proof_position == "left" {
             hash = hash_data(&format!("{}{}", sibling_hash, hash));
-            position = "right"; // Reverse the position
         } else {
             hash = hash_data(&format!("{}{}", hash, sibling_hash));
-            position = "left"; // Reverse the position
         }
-        println!("Updated Hash: {}", hash);
     }
-
+    
     let last_hash = merkle_proof.last().map(|(_, hash)| hash.to_string()).unwrap_or(hash_to_verify.to_string());
-    println!("Last Hash: {}", last_hash);
-
+    
     let verification_result = hash == last_hash;
-    println!("Verification Result: {}", verification_result);
-
     verification_result
 }
-
-
-
-
-
-
-
 
 
 fn main() -> Result<(), IoError> {
     let directory_path = "channel";
 
-    let mut content_hashes: Vec<String> = Vec::new();  
+    let mut content_hashes: Vec<String> = Vec::new();
 
     let files_to_send = scan_directory_for_files(directory_path);
 
@@ -341,7 +342,7 @@ fn main() -> Result<(), IoError> {
 
             // Hash the file content and add it to content_hashes
             let content_hash = hash_content(&file_path).unwrap();
-            
+
             content_hashes.push(content_hash);
 
             // Upload the file and handle the result
@@ -358,24 +359,18 @@ fn main() -> Result<(), IoError> {
         println!("Content Hashes: {:?}", content_hashes);
 
         // Generate the Merkle Tree from content_hashes
-        let root = generate_merkle_tree(&content_hashes);
-        println!("Root Hash: {}", root.hash);
-
-
-
-        //NEW MERKLE TREE
         let merkle_tree_new = MerkleTree::new(content_hashes.clone());
+        println!("Merkle Tree: {:?}", merkle_tree_new);
 
         // Choose an index to verify (0 in this case)
         let index_to_verify = 2;
-        //println!("Root: {:?}", root);
+
         // Generate the Merkle proof for the specified index_to_verify
-        if let Some(merkle_proof) = generate_merkle_proof(&merkle_tree_new, index_to_verify) {
+        if let Some(merkle_proof) = merkle_tree_new.generate_merkle_proof(index_to_verify) {
             println!("Merkle Proof: {:?}", merkle_proof);
 
             // Verify the hash using the Merkle proof
             let verified = verify_hash_using_proof(&merkle_proof, &content_hashes[index_to_verify]);
-            //println!("Is verified: {:?}", verified);
             if verified {
                 println!("Hash verification successful.");
             } else {
